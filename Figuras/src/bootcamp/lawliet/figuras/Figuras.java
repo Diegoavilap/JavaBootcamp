5f51cfd498fafcc3304be580ebfa7386c5ee8c17
Lawliet02 <diegoavilapava@hotmail.com>
2018-02-15 18:16:10 -0500

    Proyecto Figuras - Funcion triangulo y funcion rombo

--- Figuras/src/bootcamp/lawliet/figuras/Figuras.java
+++ Figuras/src/bootcamp/lawliet/figuras/Figuras.java
@@ -2,32 +2,120 @@ package bootcamp.lawliet.figuras;
 
 public class Figuras {
 	
+	public static final char ESPACIO = ' ';
+	public static final String SALTO_DE_LINEA = String.format("%n");
+	public static final char CARACTER = '*';
+
+	/**
+	 * Funcion que sirve para generar un cuadrado
+	 * @param 	size 
+	 * 			{@code int}
+	 * @return {@code String} 
+	 * 			 Cadena que representa un cuadrado del tamano proporcionado
+	 */
 	public static String cuadrado(int size) {
-		StringBuilder cuadrado = new StringBuilder();
-		
-		for(int filas = 1; filas <= size; ++ filas) {
-			for(int columnas = 1; columnas <= size; ++columnas) {
-				cuadrado.append("*");
-			}
-			cuadrado.append(saltoDeLinea());	
-		}
-		return cuadrado.toString();
+		return figura(size, 0, size, 0, 0);
+	}
+
+	/**
+	 * Funcion que sirve para generar un triangulo
+	 * @param 	size 
+	 * 			{@code int}
+	 * @return {@code String} 
+	 * 			 Cadena que representa un triangulo del tamano proporcionado
+	 */
+	public static String triangulo(int size) {
+		return figura(size, size - 1, 1, -1, 2);
 	}
 	
+	/**
+	 * Funcion que sirve para generar un rombo
+	 * @param 	size 
+	 * 			{@code int} Representa la la cantidad de filas que tendra el rombo hasta la mitad
+	 * @return {@code String} 
+	 * 			 Cadena que representa un rombo del tamano proporcionado
+	 */
+	public static String rombo(int size) {
+		StringBuilder builder = new StringBuilder(Figuras.triangulo(size));
+		int caracteres = ((size - 1) * 2) - 1;
+		
+		builder.append(figura(size - 1 , 1, caracteres, 1, -2));
+		return builder.toString();
+	}
+	
+	/**
+	 * Funcion que sirve para generar un triangulo
+	 * @param 	size 
+	 * 			{@code int} Tamano de la figura
+	 * @param	espacios
+	 * 			{@code int} Cantidad de espacios que tendra la figura en su primera fila
+	 * @param	caracteres
+	 * 			{@code int} Cantidad de caracteres que tendra la figura en su primera fila
+	 * @param	modificarEspacios
+	 * 			{@code int} Cuanto se va a modificar la cantidad de espacios en cada fila de la figura
+	 * @param	modificarCaracteres
+	 * 			{@code int} Cuanto se va a modificar la cantidad de caracteres en cada fila de la figura
+	 * 			
+	 * @return {@code String} 
+	 * 			 Cadena que representa una figura
+	 */
+	private static String figura(int size, int espacios, int caracteres, int modificarEspacios, int modificarCaracteres) {
+		StringBuilder builder = new StringBuilder();
+		
+		for(int filas = 1; filas <= size; ++filas) {
+			builder.append(columnas(ESPACIO, espacios))
+					.append(columnas(CARACTER, caracteres))
+					.append(SALTO_DE_LINEA);	
+			espacios += modificarEspacios;
+			caracteres += modificarCaracteres;
+		}
+		return builder.toString();
+	}
+	
+	
+	/**
+	 * Funcion que sirve para generar las columnas de una fila
+	 * @param 	c 
+	 * 			{@code char} Caracter que se va a agregar en cada columna
+	 * @param 	n 
+	 * 			{@code int} Cantidad de columnas
+	 * @return {@code String} 
+	 * 			 Cadena con un caracter por la cantidad de columnas dada  
+	 */
+	private static String columnas(char c, int n) {
+		StringBuilder builder = new StringBuilder();
+		for(int columnas = 1; columnas <= n; ++columnas) {
+			builder.append(c);
+		}
+		return builder.toString();
+	}
+	
+	/**
+	 * Funcion que sirve para generar un cuadrado estandar
+	 * @return {@code String} 
+	 * 			 Cadena que representa un cuadrado estandar que es del tamano 3
+	 */
 	public static String cuadrado() {
 		return cuadrado(3);
 	}
+	/**
+	 * Funcion que sirve para generar un triangulo estandar
+	 * @return {@code String} 
+	 * 			 Cadena que representa un triangulo estandar que es del tamano 3
+	 */
+	public static String triangulo() {
+		return triangulo(3);
+	}
 	
-	public static String triangulo(int size) {
-		StringBuilder triangulo = new StringBuilder();
-		
-		
-		
-		return triangulo.toString();
-		
+	/**
+	 * Funcion que sirve para generar un rombo estandar
+	 * @return {@code String} 
+	 * 			 Cadena que representa un rombo estandar que es del tamano 3
+	 */
+	public static String rombo() {
+		return rombo(3);
 	}
+	
 
-	public static String saltoDeLinea() {
-		return String.format("%n");
-	}
+	
 }
--- Figuras/test/bootcamp/lawliet/figuras/FigurasTest.java
+++ Figuras/test/bootcamp/lawliet/figuras/FigurasTest.java
@@ -16,6 +16,8 @@ public class FigurasTest {
 	public void init() {
 		this.builder = new StringBuilder(); 
 	}
+	
+	//Test Cuadrado
 	@Test
 	public void cuadradoSizeCero() {
 		assertThat(Figuras.cuadrado(0), is(""));
@@ -24,35 +26,36 @@ public class FigurasTest {
 	@Test 
 	public void cuadradoSizeUno() {
 		
-		builder.append("*").append(Figuras.saltoDeLinea());
+		builder.append("*").append(Figuras.SALTO_DE_LINEA);
 		assertThat(Figuras.cuadrado(1), is(builder.toString()));
 	}
 	
 	@Test
 	public void cuadradoSizeDos() {
-		builder.append("**").append(Figuras.saltoDeLinea());
-		builder.append("**").append(Figuras.saltoDeLinea());
+		builder.append("**").append(Figuras.SALTO_DE_LINEA);
+		builder.append("**").append(Figuras.SALTO_DE_LINEA);
 		assertThat(Figuras.cuadrado(2),is(builder.toString()));
 	}
 	@Test
 	public void cuadradoSizeCinco() {
-		builder.append("*****").append(Figuras.saltoDeLinea());
-		builder.append("*****").append(Figuras.saltoDeLinea());
-		builder.append("*****").append(Figuras.saltoDeLinea());
-		builder.append("*****").append(Figuras.saltoDeLinea());
-		builder.append("*****").append(Figuras.saltoDeLinea());
+		builder.append("*****").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*****").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*****").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*****").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*****").append(Figuras.SALTO_DE_LINEA);
 		assertThat(Figuras.cuadrado(5),is(builder.toString()));
 	}
 	
 	@Test
 	public void cuadradoEstandar() {
-		builder.append("***").append(Figuras.saltoDeLinea());
-		builder.append("***").append(Figuras.saltoDeLinea());
-		builder.append("***").append(Figuras.saltoDeLinea());
+		builder.append("***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("***").append(Figuras.SALTO_DE_LINEA);
 		assertThat(Figuras.cuadrado(), is(builder.toString()));
 		
 	}
 	
+	// Test Triangulo
 	@Test
 	public void trianguloSizeCero() {
 		assertThat(Figuras.triangulo(0), is(""));
@@ -60,8 +63,79 @@ public class FigurasTest {
 	
 	@Test 
 	public void trianguloSizeUno() {
-		
-		builder.append("*").append(Figuras.saltoDeLinea());
+		builder.append("*").append(Figuras.SALTO_DE_LINEA);
 		assertThat(Figuras.triangulo(1), is(builder.toString()));
 	}
+	
+	@Test 
+	public void trianguloSizeDos() {
+		builder.append(" *").append(Figuras.SALTO_DE_LINEA);
+		builder.append("***").append(Figuras.SALTO_DE_LINEA);
+		assertThat(Figuras.triangulo(2), is(builder.toString()));
+	}
+	
+	@Test 
+	public void trianguloSizeCinco() {
+		builder.append("    *").append(Figuras.SALTO_DE_LINEA);
+		builder.append("   ***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("  *****").append(Figuras.SALTO_DE_LINEA);
+		builder.append(" *******").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*********").append(Figuras.SALTO_DE_LINEA);
+		assertThat(Figuras.triangulo(5), is(builder.toString()));
+	}
+	
+	@Test 
+	public void trianguloEstandar() {
+		builder.append("  *").append(Figuras.SALTO_DE_LINEA);
+		builder.append(" ***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*****").append(Figuras.SALTO_DE_LINEA);
+		assertThat(Figuras.triangulo(3), is(builder.toString()));
+	}
+	
+	//Test Rombo
+	@Test
+	public void romboSizeCero() {
+		assertThat(Figuras.rombo(0), is(""));
+	}
+	
+	@Test 
+	public void romboSizeUno() {
+		builder.append("*").append(Figuras.SALTO_DE_LINEA);
+		assertThat(Figuras.rombo(1), is(builder.toString()));
+	}
+	
+	@Test 
+	public void romboSizeDos() {
+		builder.append(" *").append(Figuras.SALTO_DE_LINEA);
+		builder.append("***").append(Figuras.SALTO_DE_LINEA);
+		builder.append(" *").append(Figuras.SALTO_DE_LINEA);
+		assertThat(Figuras.rombo(2), is(builder.toString()));
+	}
+	
+	@Test 
+	public void romboSizeCinco() {
+		builder.append("    *").append(Figuras.SALTO_DE_LINEA);
+		builder.append("   ***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("  *****").append(Figuras.SALTO_DE_LINEA);
+		builder.append(" *******").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*********").append(Figuras.SALTO_DE_LINEA);
+		builder.append(" *******").append(Figuras.SALTO_DE_LINEA);
+		builder.append("  *****").append(Figuras.SALTO_DE_LINEA);
+		builder.append("   ***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("    *").append(Figuras.SALTO_DE_LINEA);
+		
+		assertThat(Figuras.rombo(5), is(builder.toString()));
+	}
+	
+	@Test 
+	public void romboEstandar() {
+		builder.append("  *").append(Figuras.SALTO_DE_LINEA);
+		builder.append(" ***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("*****").append(Figuras.SALTO_DE_LINEA);
+		builder.append(" ***").append(Figuras.SALTO_DE_LINEA);
+		builder.append("  *").append(Figuras.SALTO_DE_LINEA);
+		
+		assertThat(Figuras.rombo(3), is(builder.toString()));
+	}
+	
 }
